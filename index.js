3db9fb7c56428464b698eececbce0cde452844b9
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-05-15 14:44:37 -0700

    index all JSON files

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const pkg = require('../package.json');
+const pkg = require('./package.json');
+
+const opcodes = require('./opcodes.json');
+const opcodesCustom = require('./opcodes-custom.json');
+const opcodesPseudo = require('./opcodes-pseudo.json');
+const opcodesRvc = require('./opcodes-rvc.json');
+const opcodesRvcPseudo = require('./opcodes-rvc-pseudo.json');
 
 exports.pkg = pkg;
+exports.opcodes = opcodes;
+exports.opcodesCustom = opcodesCustom;
+exports.opcodesPseudo = opcodesPseudo;
+exports.opcodesRvc = opcodesRvc;
+exports.opcodesRvcPseudo = opcodesRvcPseudo;
