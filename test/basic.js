11f6afbf737410991af69a1f1c121b02c294e1ee
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-05-15 13:11:46 -0700

    initial setup

---
+++ test/basic.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const chai = require('chai');
+const lib = require('../lib/index.js');
+const expect = chai.expect;
+
+describe('basic', function () {
+    it('basic', function (done) {
+        expect(lib.pkg).to.be.an('object');
+        done();
+    });
+});
+
+/* eslint-env mocha */
