82c6216da1e9789c7199f26a785dd19a2ce46e21
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2023-03-08 00:40:38 -0800

    ansi rendering for ISA string

--- bin/riscv.js
+++ bin/riscv.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('riscv');
+const process = require('process');
+
+const { program } = require('commander');
+
+const { langParser, isaLangWith, renderAnsi } = require('../lib/index.js');
+
+const main = async () => {
+  program
+    .option('--isa <type>', 'RISC-V ISA string')
+    .parse(process.argv);
+
+  const opts = program.opts();
+
+  if (opts.isa) {
+    const parser = langParser(isaLangWith());
+    const stt = parser(opts.isa);
+    const str = renderAnsi(stt);
+    console.log(str);
+  }
+};
+
+main();
