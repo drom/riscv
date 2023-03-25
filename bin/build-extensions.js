87e525c81b0e55ce72eadb6256ebda5f77097738
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2023-03-24 17:21:18 -0700

    check opcodes duplicates and count

--- bin/build-extensions.js
+++ bin/build-extensions.js
@@ -5,8 +5,25 @@ const { writeFile } = require('fs').promises;
 
 const { opcodes, extractNode, jsonStringify } = require('../lib');
 
+const countOpcodes = (arr, name) => {
+  const o = {};
+  console.log(name);
+  arr.map((e, i) => {
+    if (o[e]) {
+      console.log('Error: Duplicate name: ' + e + ' at pos: ' + i);
+    }
+    o[e] = true;
+  });
+  console.log(Object.keys(o).length + ' files');
+};
+
+
 const main = async () => {
   const fieldo = {};
+
+  ['RATIFIED_OPCODES', 'UNRATIFIED_OPCODES'].map(e => {
+    countOpcodes(opcodes[e], e);
+  });
 
   const opcodesRatified = await extractNode(
     opcodes.ROOT,
